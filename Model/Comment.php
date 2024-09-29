31d83e428587a913732cdbdbc18922a45d60e85f
Amil <amilrampa2001@gmail.com>
2024-09-30 00:21:25 +0530

    fix issue with updated_at field not updating on model change

--- Model/Comment.php
+++ Model/Comment.php
@@ -5,15 +5,44 @@ declare(strict_types=1);
 namespace Practice\Comments\Model;
 
 use DateTimeImmutable;
+use Magento\Framework\Registry;
 use DateMalformedStringException;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Stdlib\DateTime;
 use Magento\Framework\Model\AbstractModel;
 use Practice\Comments\Api\Data\CommentInterface;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
 
 class Comment extends AbstractModel implements CommentInterface
 {
+    public function __construct(
+        Context                   $context,
+        Registry                  $registry,
+        private readonly DateTime $datetime,
+        AbstractResource          $resource = null,
+        AbstractDb                $resourceCollection = null,
+        array                     $data = []
+    ) {
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+    }
+
     protected function _construct(): void
     {
         $this->_init(ResourceModel\Comment::class);
+    }
+
+    /**
+     * @throws DateMalformedStringException
+     */
+    public function beforeSave(): Comment|static
+    {
+        parent::beforeSave();
+        if ($this->isObjectNew() && !$this->getCreatedAt()) {
+            $this->setData(self::CREATED_AT , $this->datetime->formatDate(true));
+        }
+        $this->setData(self::UPDATED_AT, $this->datetime->formatDate(true));
+        return $this;
     }
 
     public function getAuthorName(): string
@@ -66,7 +95,7 @@ class Comment extends AbstractModel implements CommentInterface
 
     public function getCommentId(): int
     {
-        return $this->getData(self::COMMENT_ID);
+        return (int)$this->getData(self::COMMENT_ID);
     }
 
     public function setCommentId(int $id): void
