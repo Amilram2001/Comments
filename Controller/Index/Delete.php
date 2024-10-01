f0663de3b77ee64a03ae4d6522f233548c2d12c9
Amil <amilrampa2001@gmail.com>
2024-10-02 00:54:27 +0530

    add controller for delete action

---
+++ Controller/Index/Delete.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Practice\Comments\Controller\Index;
+
+use Psr\Log\LoggerInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Practice\Comments\Api\CommentRepositoryInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\Message\ManagerInterface as MessageManagerInterface;
+
+class Delete implements HttpPostActionInterface
+{
+    public function __construct(
+        private readonly LoggerInterface            $logger,
+        private readonly RedirectFactory            $redirectFactory,
+        private readonly RequestInterface           $request,
+        private readonly MessageManagerInterface    $messageManager,
+        private readonly CommentRepositoryInterface $commentRepository
+    ) { }
+
+    public function execute(): ResultInterface|ResponseInterface
+    {
+        $id = $this->request->getParam('id');
+        $redirect = $this->redirectFactory->create();
+        if (!$id || !is_numeric($id)) {
+            $this->logger->critical('Missing id during delete action');
+            $this->messageManager->addErrorMessage("We can't delete this comment. Please try again later.");
+            return $redirect->setRefererUrl();
+        }
+        if (!$this->commentRepository->deleteById((int)$id)) {
+            $this->logger->critical('Comment with id ' . $id . 'not found');
+            $this->messageManager->addErrorMessage('There was an error with the request. Please refresh the page and try again.');
+            return $redirect->setRefererUrl();
+        }
+        $this->logger->info('Comment with id ' . $id . ' was successfully deleted');
+        $this->messageManager->addSuccessMessage('Successfully deleted the comment!');
+        return $redirect->setRefererUrl();
+    }
+}
