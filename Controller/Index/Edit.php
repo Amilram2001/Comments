e35e2ccb200f75ecf7c68b7be5ef25656cd7b16e
Amil <amilrampa2001@gmail.com>
2024-10-02 19:25:45 +0530

    remove unnecessary dependencies

--- Controller/Index/Edit.php
+++ Controller/Index/Edit.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace Practice\Comments\Controller\Index;
 
-use Psr\Log\LoggerInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\ResponseInterface;
 use Practice\Comments\Config\ControllerInfo;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Controller\ResultInterface;
-use Magento\Framework\Message\Manager as MessageManager;
-use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\App\Request\DataPersistorInterface;
 
@@ -19,9 +16,6 @@ class Edit implements HttpPostActionInterface
 {
     public function __construct(
         private readonly PageFactory            $pageFactory,
-        private readonly MessageManager         $manager,
-        private readonly LoggerInterface        $logger,
-        private readonly RedirectFactory        $redirectFactory,
         private readonly RequestInterface       $request,
         private readonly DataPersistorInterface $dataPersistor,
     ) { }
