0eaa6a1b91d211c4baba5af9eed7830954b0ff52
Amil <amilrampa2001@gmail.com>
2024-10-03 00:17:48 +0530

    add functionality to add a comment

---
+++ Controller/Index/Create.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Practice\Comments\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Practice\Comments\Config\ControllerInfo;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Practice\Comments\Api\CommentRepositoryInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Practice\Comments\Model\CommentFactory as CommentModelFactory;
+use Magento\Framework\Message\ManagerInterface as MessageManagerInterface;
+
+class Create implements ActionInterface
+{
+    public function __construct(
+        private readonly PageFactory                $pageFactory,
+        private readonly RedirectFactory            $redirectFactory,
+        private readonly RequestInterface           $request,
+        private readonly CommentModelFactory        $commentModelFactory,
+        private readonly MessageManagerInterface    $messageManager,
+        private readonly CommentRepositoryInterface $commentRepository
+    ) { }
+
+    public function execute(): ResultInterface|ResponseInterface
+    {
+        if (!$this->request->isPost()) {
+            return $this->pageFactory->create();
+        }
+        $name = $this->request->getParam('author_name');
+        $email = $this->request->getParam('author_email');
+        $commentContent = $this->request->getParam('comment');
+        $title = $this->request->getParam('comment_title');
+        $model = $this->commentModelFactory->create();
+        $model->setData([
+            'author_name' => $name,
+            'author_email' => $email,
+            'comment' => $commentContent,
+            'comment_title' => $title,
+        ]);
+        if ($this->commentRepository->save($model)) {
+            $this->messageManager->addSuccessMessage('Your comment has been added.');
+            $redirect = $this->redirectFactory->create();
+            return $redirect->setPath(ControllerInfo::COMMENTS_CONTROLLER_LIST_URI);
+        }
+        $this->messageManager->addErrorMessage('Your comment could not be added.');
+        return $this->redirectFactory->create()->setRefererUrl();
+    }
+}
