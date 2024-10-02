5d6504ac59da484bd7ae004ce3b595d3929c5e94
Amil <amilrampa2001@gmail.com>
2024-10-02 19:18:06 +0530

    remove validation logic from controller

--- Controller/Index/Edit.php
+++ Controller/Index/Edit.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace Practice\Comments\Controller\Index;
 
-use Magento\Framework\App\Request\DataPersistorInterface;
-use Practice\Comments\Config\ControllerInfo;
 use Psr\Log\LoggerInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\ResponseInterface;
+use Practice\Comments\Config\ControllerInfo;
 use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Message\Manager as MessageManager;
 use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\Request\DataPersistorInterface;
 
 class Edit implements HttpPostActionInterface
 {
@@ -28,12 +28,7 @@ class Edit implements HttpPostActionInterface
 
     public function execute(): ResultInterface|ResponseInterface
     {
-        $id = $this->request->getParam('id') ?? '';
-        if (!$id || !is_numeric($id)) {
-            $this->manager->addErrorMessage(__('Invalid request parameters.'));
-            $this->logger->critical(__('Invalid request parameters. Id should be an integer; Received %1', $id));
-            return $this->redirectFactory->create()->setRefererUrl();
-        }
+        $id = $this->request->getParam('id');
         $this->dataPersistor->set(ControllerInfo::CURRENT_COMMENT_ID, $id);
         return $this->pageFactory->create();
     }
