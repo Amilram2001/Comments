b5a0318591b34b89a4c7c8a4c75717e024051c33
Amil <amilrampa2001@gmail.com>
2024-10-03 15:58:13 +0530

    dispatch event after saving is successful

--- Controller/Index/Create.php
+++ Controller/Index/Create.php
@@ -13,6 +13,7 @@ use Magento\Framework\Controller\ResultInterface;
 use Practice\Comments\Api\CommentRepositoryInterface;
 use Magento\Framework\Controller\Result\RedirectFactory;
 use Practice\Comments\Model\CommentFactory as CommentModelFactory;
+use Magento\Framework\Event\ManagerInterface as EventManagerInterface;
 use Magento\Framework\Message\ManagerInterface as MessageManagerInterface;
 
 class Create implements ActionInterface
@@ -21,6 +22,7 @@ class Create implements ActionInterface
         private readonly PageFactory                $pageFactory,
         private readonly RedirectFactory            $redirectFactory,
         private readonly RequestInterface           $request,
+        private readonly EventManagerInterface      $eventManager,
         private readonly CommentModelFactory        $commentModelFactory,
         private readonly MessageManagerInterface    $messageManager,
         private readonly CommentRepositoryInterface $commentRepository
@@ -43,6 +45,7 @@ class Create implements ActionInterface
             'comment_title' => $title,
         ]);
         if ($this->commentRepository->save($model)) {
+            $this->eventManager->dispatch(ControllerInfo::EVENT_CREATE_COMMENT, ['comment' => $model]);
             $this->messageManager->addSuccessMessage('Your comment has been added.');
             $redirect = $this->redirectFactory->create();
             return $redirect->setPath(ControllerInfo::COMMENTS_CONTROLLER_LIST_URI);
