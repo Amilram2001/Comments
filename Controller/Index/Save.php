778466e442f1b710bdf9a22e33cec99faca169f7
Amil <amilrampa2001@gmail.com>
2024-10-03 13:58:15 +0530

    remove validation logic from controller

--- Controller/Index/Save.php
+++ Controller/Index/Save.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Practice\Comments\Controller\Index;
 
-use Magento\Framework\App\ResponseInterface;
-use Magento\Framework\Controller\ResultInterface;
-use Practice\Comments\Config\ControllerInfo;
 use Psr\Log\LoggerInterface;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Practice\Comments\Config\ControllerInfo;
 use Practice\Comments\Api\Data\CommentInterface;
+use Magento\Framework\Controller\ResultInterface;
 use Practice\Comments\Api\CommentRepositoryInterface;
 use Magento\Framework\Message\Manager as MessageManager;
 use Magento\Framework\Controller\Result\RedirectFactory;
@@ -27,31 +27,19 @@ class Save implements HttpPostActionInterface
 
     public function execute(): ResponseInterface|ResultInterface
     {
-        $id = $this->request->getParam('comment_id');
-        $name = $this->request->getParam('author_name');
-        $email = $this->request->getParam('author_email');
+        $id             = $this->request->getParam('comment_id');
+        $name           = $this->request->getParam('author_name');
+        $email          = $this->request->getParam('author_email');
+        $title          = $this->request->getParam('comment_title');
         $commentContent = $this->request->getParam('comment');
-        $title = $this->request->getParam('comment_title');
 
-        if (!$id || !is_numeric($id)) {
-            $errorMessage = __('Invalid ID. Expecting an integer. Received %1', $id);
-            $this->messageManager->addErrorMessage($errorMessage);
-            $this->logger->debug($errorMessage);
-            return $this->redirectFactory->create()->setRefererUrl();
-        }
-        if (empty($commentContent) || empty($title) || empty($email) || empty($name)) {
-            $errorMessage = __('Invalid comment content, title, email, name or all');
-            $this->messageManager->addErrorMessage($errorMessage);
-            $this->logger->debug($errorMessage);
-            return $this->redirectFactory->create()->setRefererUrl();
-        }
         /** @var CommentInterface $comment */
         $id = (int)$id;
         $comment = $this->commentRepository->getById($id);
         if (!$comment) {
             $this->messageManager->addErrorMessage(__('Comment with id %1 not found.', $id));
             $this->logger->debug(__('Comment with id %1 not found.', $id));
-            return $this->redirectFactory->create()->setRefererUrl();
+            return $this->redirectFactory->create()->setPath(ControllerInfo::COMMENTS_CONTROLLER_LIST_URI);
         }
 
         $comment->setAuthorName($name);
