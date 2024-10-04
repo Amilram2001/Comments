4ed0ca49c1a3cf603a09c8c1d5a6498388018d4e
Amil <amilrampa2001@gmail.com>
2024-10-04 21:15:08 +0530

    add update event name

--- Config/ControllerInfo.php
+++ Config/ControllerInfo.php
@@ -9,4 +9,5 @@ enum ControllerInfo: string
     const string CURRENT_COMMENT_ID = 'current_comment_id';
     const string COMMENTS_CONTROLLER_LIST_URI = 'comments/index/index';
     const string EVENT_CREATE_COMMENT = 'practice_comments_after_create_comment';
+    const string EVENT_UPDATE_COMMENT = 'practice_comments_after_update_comment';
 }
